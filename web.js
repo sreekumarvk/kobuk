0d215bd2e7e788d00854ebd0d9a54103a44093c8
Sreekumar V. Kodakara <sreekumar.v.kodakara@intel.com>
2012-11-27 21:36:40 -0800

    Added routes.

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
 var express = require('express');
+var dashboard = require('./routes/dashboard');
 
-var app = express.createServer(express.logger());
-
-app.get('/' , function(request, response){
-	response.send('Hello Mangaboys!');
-
-});
+var app = module.exports = express.createServer(express.logger());
 
 var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
+
+app.get('/dashboard',dashboard.home);
+app.get('/dashboard/:id',dashboard.user);
