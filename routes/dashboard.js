c834695f4cb8f495740414e9bfcac7b75696fbb5
Rangaprabhu Parthasarathy <rangaprabhu.dev@gmail.com>
2012-12-01 15:02:20 -0800

    Updating css pointers
    Changes css lookup in layout.jade.
    Also updated dashboard.jade and dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,21 @@
+// exports.home = function(request,response) {
+//  response.send('This is dashboard');
+// }
+
 exports.home = function(request,response) {
-  response.send('In dashboards');
+	var data = {
+		title : "Qontaqt.Me",
+		id : request.params.id
+
+	};
+  response.render("dashboard", data);
 }
 
-exports.user = function(request,response) {
-    var data = {
-        title : "Profile",
-        id : request.params.id
-    };
-    response.render("user", data);
-}
+//prabhu@3:00pm: Commenting this section as it is not needed anymore
+// exports.user = function(request,response) {
+//     var data = {
+//         title : "Profile",
+//         id : request.params.id
+//     };
+//     response.render("user", data);
+// }
