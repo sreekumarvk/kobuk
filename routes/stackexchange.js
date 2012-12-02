30ff9065c82b880c3f908e8f3ca8af62e9fd35cd
Pradeep <pradmail@gmail.com>
2012-12-02 11:33:17 -0800

    stackexchange api call fixed with apikey

--- routes/stackexchange.js
+++ routes/stackexchange.js
@@ -29,15 +29,41 @@ function getGzipped(url, callback) {
 }
 
 var reputations = function(req, res) {
-    getGzipped('http://api.stackexchange.com/2.1/users/22656/reputation?site=stackoverflow', function(err, data) {
-        if (err) {
-            console.log("reputations ERROR " + err);
-            res.send('ERROR' + JSON.stringify(err));
-            return;
+    // initialize
+    var apikey = 'vd)IDQrxymScSe9QQ*blKw((';
+    var userid = 22656;
+    var apiurl = 'http://api.stackexchange.com/2.1/users/' + userid + '/badges?key=' + apikey + '&order=desc&sort=rank&site=stackoverflow';
+
+    // decompress
+    getGzipped(apiurl, function(err, data) {
+        if (err){
+            console.log("Error returned by badge api: " + err);
         }
+
+        // convert to json object
         var jsobj = JSON.parse(data);
-        console.log(jsobj);
-        res.render("stackexchange", {'title' : "stackexchange", 'stackexchange': jsobj});
+
+        // get user info from returned json
+        var user_name = jsobj.items[0].user.display_name;
+        var reputation = jsobj.items[0].user.reputation;
+        var accept_rate = jsobj.items[0].user.accept_rate;
+
+        console.log("****** username: " + user_name + ", reputation: " + reputation + ", accept_rate: " + accept_rate + ". ******");
+
+        // get expertise
+        // for (var i in jsobj){
+        //     var rank = jsobj.items[i].rank;
+        //     var subject = jsobj.items[i].rank;
+
+        //     console.log("++++++ rank: " + rank + ", subject: " + subject + ". ++++++");
+        // }
+
+        // construct return json
+
+
+        // return to client
+        console.log(jsobj.items[0].user.display_name);
+        res.render("stackexchange", {'title' : "Stackexchange Badges", 'stackexchange': jsobj});
     });
 }
 
