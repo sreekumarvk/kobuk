47e803433a316909bf601af5bfb4249fd5bdb0b1
Manoj Ananth <manoj@beckon.com>
2012-12-01 17:17:51 -0800

    basic twitter api support

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var dashboard = require('./routes/dashboard');
+var twitter = require('./routes/twitter');
+
 
 var app = module.exports = express.createServer(express.logger());
 
@@ -18,12 +20,31 @@ app.configure(function(){
     app.use(express.static(__dirname + '/static'));
 });
 
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.logger());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: "very secret" }));
+});
+
+
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
 
 app.get('/dashboard',dashboard.home);
 app.get('/dashboard/:id',dashboard.user);
+
+app.get('/twitter/connect', twitter.connect);
+app.get('/twitter/lookup/:screen_name', twitter.lookup);
+
+app.get('/')
+
+app.dynamicHelpers({
+    session: function(req, res){
+        return req.session;
+    }
+});
 
 
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
