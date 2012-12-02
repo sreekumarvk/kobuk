1d2e4778da4b6e9b6a98feb50de932697450a9f6
Manoj Ananth <manoj@beckon.com>
2012-12-01 21:02:57 -0800

    stackexchange changes

--- routes/stackexchange.js
+++ routes/stackexchange.js
@@ -2,12 +2,14 @@ var http = require("http"),
     zlib = require("zlib");
 
 function getGzipped(url, callback) {
+//    callback(null, JSON.stringify({'items':[{reputations_count:10}]}));
+//    return;
     // buffer to store the streamed decompression
     var buffer = [];
 
     http.get(url, function(res) {
         // pipe the response into the gunzip to decompress
-        var gunzip = zlib.createGunzip();            
+        var gunzip = zlib.createGunzip();
         res.pipe(gunzip);
 
         gunzip.on('data', function(data) {
@@ -16,7 +18,7 @@ function getGzipped(url, callback) {
 
         }).on("end", function() {
             // response and decompression complete, join the buffer and return
-            callback(null, buffer.join("")); 
+            callback(null, buffer.join(""));
 
         }).on("error", function(e) {
             callback(e);
@@ -26,9 +28,19 @@ function getGzipped(url, callback) {
     });
 }
 
-exports.reputations = function(req, res) {
+var reputations = function(req, res) {
     getGzipped('http://api.stackexchange.com/2.1/users/22656/reputation?site=stackoverflow', function(err, data) {
+        if (err) {
+            console.log("reputations ERROR " + err);
+            res.send('ERROR' + JSON.stringify(err));
+            return;
+        }
         var jsobj = JSON.parse(data);
-        //response.render("stackexchange", data);                
+        console.log(jsobj);
+        res.render("stackexchange", {'title' : "stackexchange", 'stackexchange': jsobj});
     });
 }
+
+module.exports = {
+    reputations: reputations
+};
